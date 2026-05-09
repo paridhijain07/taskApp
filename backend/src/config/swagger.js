7f6e1d5c3ae7b23f7997225b05d68887485639b0
paridhijain07 <positiveparidhi@gmail.com>
2026-05-09 16:13:09 +0530

    .

--- backend/src/config/swagger.js
+++ backend/src/config/swagger.js
@@ -13,7 +13,7 @@ const swaggerSpec = swaggerJSDoc({
       {
         url:
           process.env.NODE_ENV === 'production'
-            ? 'https://taskapp-1-cbic.onrender.com'
+            ? 'https://taskapp-j73b.onrender.com'
             : `http://localhost:${process.env.PORT || 5000}`,
       },
     ],
